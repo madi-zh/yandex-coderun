f1263e5a7f649fdd63e5d9e488275dc185121d83
Madi Zhanbyrtayev <madi.zhanbyrtayev@nexxiot.com>
2024-10-29 13:54:02 +0500

    docs: add solutions to the cheapest-way adn median-out-of-three problems

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # yandex-coderun
 Just a repo for python solutions to yandex coderun problems
+
+Want to keep a .py versions of solutions for future reference
---
+++ cheapest-way/main.py
@@ -0,0 +1,30 @@
+import sys
+
+
+def main():
+    
+    N, M = [int(i) for i in input().split(" ")]
+    
+    weights = []
+    for _ in range(N):
+        row_input = [int(i) for i in sys.stdin.readline().strip("\r\n").split(" ")]
+        weights.append(row_input)
+    if N == 1 and M == 1:
+        print(weights[0][0])
+        return
+
+    costs = [[0]* M for i in range(N)]
+    for row in range(N):
+        for col in range(M):
+            if row == 0 and col == 0:
+                costs[row][col] = weights[row][col]
+            elif row == 0 and col > 0:
+                costs[row][col] = costs[row][col-1] + weights[row][col]
+            elif row > 0 and col == 0:
+                costs[row][col] = costs[row-1][col]+ weights[row][col]
+            else:
+                costs[row][col] = min(costs[row-1][col]+weights[row][col], costs[row][col-1]+weights[row][col])
+    print(costs[N-1][M-1])
+
+if __name__ == '__main__':
+    main()
